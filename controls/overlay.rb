8d857166c056c7530c3dc0f1d8e3d7b137175bc6
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2022-08-30 14:48:22 -0400

    Update overlay.rb

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,36 +1,27 @@
-overlay_controls = input('overlay_controls')
-
 include_controls 'oracle-mysql-ee-5.7-cis-baseline' do
 
-    control '1.1' do
-        tag "nist": ['SC-1']
+  control '1.2' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable for our project since we have an approved risk-based decision on 10/1/2021 allowing use of root account for running mysql.'
+    describe 'This is Not Applicable for our project since we have an approved risk-based decision on 10/1/2021 allowing use of root account for running mysql.' do
+      skip 'This is Not Applicable for our project since we have an approved risk-based decision on 10/1/2021 allowing use of root account for running mysql.'
     end
+  end
 
-    control '1.2' do
-        impact 0.0
-        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
-        describe 'This control is not applicable when running MySQL on Microsoft systems' do
-            skip 'This control is not applicable when running MySQL on Microsoft systems'
-        end
+  control '7.1' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable for our project since we have an approved risk-based decision on 10/1/2021 for backwards compatibility.'
+    describe 'This is Not Applicable for our project since we have an approved risk-based decision on 10/1/2021 for backwards compatibility.' do
+      skip 'This is Not Applicable for our project since we have an approved risk-based decision on 10/1/2021 for backwards compatibility.'
     end
+  end
 
-    control '2.1' do
-        only_if { os.linux? or os.windows? }
+  control '8.1' do
+    impact 0.0
+    desc 'caveat', 'This is Not Applicable for our project since we network traffic travels only over internal trusted networks.'
+    describe 'This is Not Applicable for our project since we network traffic travels only over internal trusted networks.' do
+      skip 'This is Not Applicable for our project since we network traffic travels only over internal trusted networks.'
     end
-
-    control '3.1' do
-        describe "A manual review is required to verify Ensure 'datadir' Has Appropriate Permissions and Ownership. If it is then this is a finding." do	
-         skip "A manual review is required to verify Ensure 'datadir' Has Appropriate Permissions and Ownership. If it is then this is a finding."	
-        end
-    end
-
-    control '4.1' do
-        title "Ensure Latest Security Patches Are Applied are greater than the version 5.7.x"
-        tag "check": "
-  Execute the following SQL statement to identify the MySQL server version:
-    SHOW VARIABLES WHERE Variable_name LIKE 'version';
-  Now compare the version with the security announcements from Oracle and/or the OS if the OS packages are used"
-  tag "fix": 'Install the latest patches for your version or upgrade to the latest version'
-    end
+  end
 
 end
